0bdab2e579d4325738368d04cf8f2a4251a1fd91
peggy <pei.wang@epfl.ch>
2018-12-08 12:44:44 +0800

    rewrite utils functions. rewrite gbm in ipynb

--- utils.py
+++ utils.py
@@ -5,37 +5,25 @@ import pandas as pd
 
 class FileReader(object):
     def __init__(self):
-        self.srcs = []
-        self.src = None
-        self.names = []
-        self.name = None
+        self.src = []
+        self.name = []
 
-    def load_csv_file(self, path: str, name: str):
-        print("Loading file: %s" % (name), file=sys.stderr)
-        return pd.read_csv(path)
-    
-    def load_csv_file_batch(self, paths: list, names: list):
+    def load_csv_file(self, paths, names):
         data = {}
         for path, name in zip(paths, names):
-            data[name] = self.load_csv_file(path, name)
+            print("Loading file: %s" % (name))
+            data[name] = pd.read_csv(path)
         return data
 
-    def load_file(self, _input, is_batch=False):
-        if is_batch:
-            self.srcs = _input
-            self.names = []
-            for src in self.srcs:
-                self.src = src
-                self.name = self.src.split('/')[-1].split('.')[0]
-                self.names.append(self.name)
-            return self.load_csv_file_batch(self.srcs, self.names)
-        else:
+    def load_file(self, _input):
+        if isinstance(_input, list):
             self.src = _input
-            self.name = self.name = self.src.split('/')[-1].split('.')[0]
-            return  self.load_csv_file(self.src, self.name)
-
-
-if __name__ == '__main__':
-    reader = FileReader()
-    data = reader.load_file('./data/train.csv')
-    data = reader.load_file(['./data/train.csv', './data/merchants.csv'], is_batch=True)
+        elif isinstance(_input, str):
+            self.src = [_input]
+        else:
+            print("Unsuppotred path format. ")
+            return -1
+        for src in self.src:
+            name = src.split('/')[-1].split('.')[0]
+            self.name.append(name)
+        return(self.load_csv_file(self.src, self.name))
